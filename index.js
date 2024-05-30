0be44bd9a5445b241f0f233e981c76d31de2cc6d
R.A. te Boekhorst <rateboekhorst@mailbox.org>
2024-05-30 17:29:59 +0200

    fix: adding env and stuff to have this initially running

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+// Importing necessary libraries
+const config = require('dotenv').config();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dns = require('dns');
+const mongoose = require('mongoose');
+
+const dns = require('dns');
+const mongoose = require('mongoose');
+
+// Basic Configurations
+// Assign port if not set in env
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,10 +20,15 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+// Wait for Mongo database to connect, logging an error if there is a problem
+dbmain().catch((err) => {
+  console.error('Database connection error:', err);
+  process.exit(1);
+});
 
 
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(port || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
